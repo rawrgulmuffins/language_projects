634ecc43ee68d9dc413329ec772f33c65b98653b
Alex Lord <lostdogs101@gmail.com>
2015-12-27 12:00:28 -0800

    Finished first chapter. Need to understand code.

--- compiler/reference_compilers/lets_make_a_compiler/README.md
+++ compiler/reference_compilers/lets_make_a_compiler/README.md
@@ -1,2 +1,11 @@
 # Reference Material
 http://compilers.iecc.com/crenshaw/
+
+# Notes On Introduction
+## FPC
+* OSX needs the free pascal compiler which is almost equivilent to turbo pascal.
+* http://www.toosquare.com/2014/07/install-pascal-on-os-x/
+## Assembly language 
+* 68000 code is the assembly language used.
+* TOOD: Need to figure out what's used on osx Yosemite.
+* TODO: Translate from 68000 to the assembly for my laptop.
---
+++ compiler/reference_compilers/lets_make_a_compiler/cradle.pas
@@ -0,0 +1,138 @@
+{--------------------------------------------------------------}
+program Cradle;
+
+{--------------------------------------------------------------}
+{ Constant Declarations }
+
+const TAB = ^I;
+
+{--------------------------------------------------------------}
+{ Variable Declarations }
+
+var Look: char;              { Lookahead Character }
+
+{--------------------------------------------------------------}
+{ Read New Character From Input Stream }
+
+procedure GetChar;
+begin
+   Read(Look); { One character at a time }
+end;
+
+{--------------------------------------------------------------}
+{ Report an Error }
+
+procedure Error(s: string);
+begin
+   WriteLn; { assuming this means write a single new line? }
+   WriteLn(^G, 'Error: ', s, '.'); { Not sure what ^G is. }
+end;
+
+
+{--------------------------------------------------------------}
+{ Report Error and Halt }
+
+procedure Abort(s: string);
+begin
+   Error(s);
+   Halt; { No exit code? }
+end;
+
+
+{--------------------------------------------------------------}
+{ Report What Was Expected }
+
+procedure Expected(s: string);
+begin
+   Abort(s + ' Expected');
+end;
+
+{--------------------------------------------------------------}
+{ Match a Specific Input Character }
+
+procedure Match(x: char);
+begin
+   if Look = x then GetChar
+   else Expected('''' + x + '''');
+end;
+
+
+{--------------------------------------------------------------}
+{ Recognize an Alpha Character }
+
+function IsAlpha(c: char): boolean;
+begin
+   IsAlpha := upcase(c) in ['A'..'Z'];
+end;
+                              
+
+{--------------------------------------------------------------}
+
+{ Recognize a Decimal Digit }
+
+function IsDigit(c: char): boolean;
+begin
+   IsDigit := c in ['0'..'9'];
+end;
+
+
+{--------------------------------------------------------------}
+{ Get an Identifier }
+
+function GetName: char;
+begin
+   if not IsAlpha(Look) then Expected('Name');
+   GetName := UpCase(Look);
+   GetChar;
+end;
+
+
+{--------------------------------------------------------------}
+{ Get a Number }
+
+function GetNum: char;
+begin
+   if not IsDigit(Look) then Expected('Integer');
+   GetNum := Look;
+   GetChar;
+end;
+
+
+{--------------------------------------------------------------}
+{ Output a String with Tab }
+
+procedure Emit(s: string);
+begin
+   Write(TAB, s);
+end;
+
+
+
+
+{--------------------------------------------------------------}
+{ Output a String with Tab and CRLF }
+
+procedure EmitLn(s: string);
+begin
+   Emit(s);
+   WriteLn;
+end;
+
+{--------------------------------------------------------------}
+{ Initialize }
+
+procedure Init;
+begin
+   GetChar;
+end;
+
+
+{--------------------------------------------------------------}
+{ Main Program }
+
+begin
+   Init;
+end.
+{--------------------------------------------------------------}
+
+
