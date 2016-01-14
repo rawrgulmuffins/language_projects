b7c101e73a45ffed56d1f78efa7de4f73bc8a4eb
Alex Lord <lostdogs101@gmail.com>
2016-01-14 15:17:51 -0800

    Worked out the assembly for addition and subtraction.

--- compiler/reference_compilers/lets_make_a_compiler/assembly_explorations/algebra/addition.s
+++ compiler/reference_compilers/lets_make_a_compiler/assembly_explorations/algebra/addition.s
@@ -1,8 +1,10 @@
 .text
 .globl _main
 _main:
-  subq $8, %rsp
-  movq $0, %rax
-  movq %rax, %rbx
-  movq $0, %rdi
-  call _exit
+    subq $8, %rsp
+    movq $1, %rax
+    movq %rax, %rbx
+    movq $2, %rax
+    add %rax, %rbx
+    movq $0, %rdi
+    call _exit
--- compiler/reference_compilers/lets_make_a_compiler/assembly_explorations/algebra/subtraction.s
+++ compiler/reference_compilers/lets_make_a_compiler/assembly_explorations/algebra/subtraction.s
@@ -1,8 +1,10 @@
 .text
 .globl _main
 _main:
-  subq $8, %rsp
-  movq $0, %rax
-  movq %rax, %rbx
-  movq $0, %rdi
-  call _exit
+    subq $8, %rsp
+    movq $2, %rax
+    movq %rax, %rbx
+    movq $1, %rax
+    sub %rax, %rbx
+    movq $0, %rdi
+    call _exit
