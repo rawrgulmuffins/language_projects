b151d1aa8da1734e403bf5593138888a2ca51184
Alex Lord <lostdogs101@gmail.com>
2016-01-17 21:25:22 -0800

    Finally figured out how divison works.

--- compiler/reference_compilers/lets_make_a_compiler/assembly_explorations/algebra/division.s
+++ compiler/reference_compilers/lets_make_a_compiler/assembly_explorations/algebra/division.s
@@ -2,10 +2,14 @@
 .globl _main
 _main:
     subq $8, %rsp
-    movq $1, %rax
-    push %rax
     movq $2, %rax
-    pop %rbx
-    add %rax, %rbx
+    push %rax
+    movq $1, %rax
+    movq %rax, %rbx
+    pop %rax
+    # Took waaay to long to figure this out. Information at
+    # https://stackoverflow.com/questions/8858104/64bit-nasm-division-idiv
+    xor  %rdx, %rdx # clear high bits of dividend 
+    div %rbx
     movq $0, %rdi
     call _exit
