6f2bd4c9fad1073e9c9052ed0192de072cd671ae
Alex Lord <lostdogs101@gmail.com>
2016-01-18 00:22:59 -0800

    Standardized docs formatting and added untrasnlated multiplication and division functions.

--- compiler/reference_compilers/lets_make_a_compiler/cradle.pas
+++ compiler/reference_compilers/lets_make_a_compiler/cradle.pas
@@ -12,7 +12,7 @@ const TAB = ^I;
 {--------------------------------------------------------------}
 { Variable Declarations }
 
-var Look: char;              { Lookahead Character }
+var Look: char; { Lookahead Character }
 
 {--------------------------------------------------------------}
 { Read New Character From Input Stream }
@@ -24,27 +24,22 @@ end;
 
 {--------------------------------------------------------------}
 { Report an Error }
-
 procedure Error(s: string);
 begin
     WriteLn; { assuming this means write a single new line? }
     WriteLn(^G, 'Error: ', s, '.'); { Not sure what ^G is. }
 end;
 
-
 {--------------------------------------------------------------}
 { Report Error and Halt }
-
 procedure Abort(s: string);
 begin
     Error(s);
     Halt; { No exit code? }
 end;
 
-
 {--------------------------------------------------------------}
 { Report What Was Expected }
-
 procedure Expected(s: string);
 begin
     Abort(s + ' Expected');
@@ -52,36 +47,28 @@ end;
 
 {--------------------------------------------------------------}
 { Match a Specific Input Character }
-
 procedure Match(x: char);
 begin
     if Look = x then GetChar
     else Expected('''' + x + '''');
 end;
 
-
 {--------------------------------------------------------------}
 { Recognize an Alpha Character }
-
 function IsAlpha(c: char): boolean;
 begin
     IsAlpha := upcase(c) in ['A'..'Z'];
 end;
-                              
 
 {--------------------------------------------------------------}
-
 { Recognize a Decimal Digit }
-
 function IsDigit(c: char): boolean;
 begin
     IsDigit := c in ['0'..'9'];
 end;
 
-
 {--------------------------------------------------------------}
 { Get an Identifier }
-
 function GetName: char;
 begin
     if not IsAlpha(Look) then Expected('Name');
@@ -89,10 +76,8 @@ begin
     GetChar;
 end;
 
-
 {--------------------------------------------------------------}
 { Get a Number }
-
 function GetNum: char;
 begin
     if not IsDigit(Look) then Expected('Integer');
@@ -100,19 +85,15 @@ begin
     GetChar;
 end;
 
-
 {--------------------------------------------------------------}
 { Output a String with Tab }
-
 procedure Emit(s: string);
 begin
     Write(TAB, s);
 end;
 
-
 {--------------------------------------------------------------}
 { Output a String with Tab and CRLF }
-
 procedure EmitLn(s: string);
 begin
     Emit(s);
@@ -121,7 +102,6 @@ end;
 
 {--------------------------------------------------------------}
 { Initialize }
-
 procedure Init;
 begin
     { Grab the next character. Not doing token lookahead yet.}
@@ -133,6 +113,8 @@ begin
     EmitLn('    subq $8, %rsp');
 end;
 
+{--------------------------------------------------------------}
+{ After expression parsing boiler plate. }
 procedure Post;
 begin
     { Post Program teardown. Always report program success for now. }
@@ -141,17 +123,48 @@ begin
 end;
 
 {---------------------------------------------------------------}
-{ Parse and Translate an Expression }
+{ Parse and Translate a Math Factor }
+procedure Factor;
+begin
+    EmitLn('MOVE #' + GetNum + ',D0')
+end;
+
+{--------------------------------------------------------------}
+{ Recognize and Translate a Multiply }
+procedure Multiply;
+begin
+    Match('*');
+    Factor;
+    EmitLn('MULS (SP)+,D0');
+end;
+
+{-------------------------------------------------------------}
+{ Recognize and Translate a Divide }
+procedure Divide;
+begin
+    Match('/');
+    Factor;
+    EmitLn('MOVE (SP)+,D1');
+    EmitLn('DIVS D1,D0');
+end;
+
+{---------------------------------------------------------------}
+{ Parse and Translate a Math Term }
 procedure Term;
 begin
-    EmitLn('    movq $' + GetNum + ', %rax')
+Factor;
+    while Look in ['*', '/'] do begin
+        EmitLn('MOVE D0,-(SP)');
+        case Look of
+        '*': Multiply;
+        '/': Divide;
+        else Expected('Mulop');
+        end;
+    end;
 end;
-{---------------------------------------------------------------}
-
 
 {--------------------------------------------------------------}
 { Recognize and Translate an Add }
-
 procedure Add;
 begin
     Match('+');
@@ -160,10 +173,8 @@ begin
     EmitLn('    add %rax, %rbx');
 end;
 
-
 {-------------------------------------------------------------}
 { Recognize and Translate a Subtract }
-
 procedure Subtract;
 begin
     Match('-');
@@ -173,12 +184,9 @@ begin
     EmitLn('    pop %rbx');
     EmitLn('    sub %rax, %rbx');
 end;
-{-------------------------------------------------------------}
-
 
 {---------------------------------------------------------------}
 { Parse and Translate an Expression }
-
 procedure Expression;
 begin
     Term;
@@ -191,11 +199,9 @@ begin
         end;
     end;
 end;
-{--------------------------------------------------------------}
 
 {--------------------------------------------------------------}
 { Main Program }
-
 begin
     Init;
     Expression;
