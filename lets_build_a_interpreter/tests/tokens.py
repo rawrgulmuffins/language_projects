8cb9a0ad1255be3b8f728ebd74f32331cd305ebc
Alex Lord <lostdogs101@gmail.com>
2016-08-23 15:31:51 -0700

    renamed base module file.

--- lets_build_a_interpreter/tests/tokens.py
+++ lets_build_a_interpreter/tests/tokens.py
@@ -5,26 +5,25 @@ the interpreter doesn't have any data validation as a feature.
 """
 import pytest
 
-import interpreter
-from interpreter import Token
+import pascal_interpreter as augur
 
 def test_no_defaults():
     # There's no valid defaults at the moment.
     with pytest.raises(TypeError):
-        interpreter.Token()
+        augur.Token()
 
 def test_known_type():
     # There's no valid defaults at the moment.
-    token = interpreter.Token(type=interpreter.INTEGER, value=2)
+    token = augur.Token(type=augur.INTEGER, value=2)
     assert token.value == 2
-    assert token.type == interpreter.INTEGER
+    assert token.type == augur.INTEGER
 
 def test_str_non_string_value():
-    token = interpreter.Token(type=interpreter.INTEGER, value=2)
+    token = augur.Token(type=augur.INTEGER, value=2)
     expected_result = "Token(type=INTEGER, value=2)"
     assert token.__str__() == expected_result
 
 def test_repr():
-    token = interpreter.Token(type=interpreter.INTEGER, value=2)
+    token = augur.Token(type=augur.INTEGER, value=2)
     expected_result = "Token(type=INTEGER, value=2)"
     assert token.__repr__() == expected_result
