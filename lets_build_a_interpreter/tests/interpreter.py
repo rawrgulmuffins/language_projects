b09d2fa1a4fa28db3ab29b170f12d593c498998b
Alex Lord <lostdogs101@gmail.com>
2016-08-23 15:38:11 -0700

    Renamed augur to p_interp.

--- lets_build_a_interpreter/tests/interpreter.py
+++ lets_build_a_interpreter/tests/interpreter.py
@@ -1,17 +1,17 @@
 import pytest
 
-import pascal_interpreter as augur
+import pascal_interpreter as p_interp
 
 
 def test_interpreter_blank_program():
     input_text = ""
-    interp = augur.Interpreter(text=input_text)
+    interp = p_interp.Interpreter(text=input_text)
     # If this doesn't barf we're good.
 
 
 def test_interpreter_error_type():
     input_text = ""
-    interpreter = augur.Interpreter(text=input_text)
-    augur.InterpreterError
-    with pytest.raises(augur.InterpreterError):
+    interpreter = p_interp.Interpreter(text=input_text)
+    p_interp.InterpreterError
+    with pytest.raises(p_interp.InterpreterError):
         interpreter._error()
