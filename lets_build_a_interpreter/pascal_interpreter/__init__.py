42f04495a7e0e9804b6c04b3362ce8df45d5fc0d
Alex Lord <lostdogs101@gmail.com>
2016-09-05 23:12:30 -0700

    Changing the structure to allow for TravisCI builds.

---
+++ lets_build_a_interpreter/pascal_interpreter/__init__.py
@@ -0,0 +1,2 @@
+from . pascal_interpreter import INTEGER, EOF, PLUS, Token, Interpreter
+from . pascal_interpreter import InterpreterError
---
+++ lets_build_a_interpreter/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with open("build_requires.txt") as file:
+        install_requires = [dep.strip() for dep in file.readlines()]
+except FileNotFoundError:
+    # If we're installing from the top directory in language projects..
+    with open("lets_build_a_interpreter/build_requires.txt") as file:
+        install_requires = [dep.strip() for dep in file.readlines()]
+
+
+setup(
+    name='pascal_interpreter',
+    description='Toy Pascal Interpreter.',
+    version='0.0.1',
+    long_description='',
+    url='https://github.west.isilon.com/alord/scatter_gather_grep',
+    author='Alex LordThorsen',
+    author_email='alord@isilon.com',
+    license='ISCL',
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Business Information',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='',
+    packages=find_packages(include=['pascal_interpreter']),
+    py_modules=[],
+    install_requires=install_requires,
+)
